0c830e07c6369c00b69e988164116d372750c3c9
vichi100 <vichi100@gmail.com>
2021-07-31 08:37:03 +0530

    menu

--- models/menu.js
+++ models/menu.js
@@ -1,4 +1,4 @@
-menu = {
+const menu = {
     id: String,
     restaurent_id: String,
     restaurent_name: String,
@@ -6,10 +6,31 @@ menu = {
         {
             item_id: String,
             item_name: String,
+            is_customizable: String, // yes no
             price_details: [
                 {
                     quantity: Number,// full, half, portion, 1kg, 1/2kg
                     price: Number,
+                }
+            ],
+
+            customization_details: [
+                {
+                    header: String,
+                    children_type: "radio", // checkbox
+                    children: [
+                        {
+                            name: String,
+                            price: String,
+                        }
+                    ],
+
+                },
+                {
+                    header: String,
+                    children_type: "checkbox", // checkbox
+                    children: [],
+
                 }
             ],
 
@@ -31,9 +52,31 @@ menu = {
                     price: Number,
                 }
             ],
+            customization_details: [
+                {
+                    header: String,
+                    children_type: "radio", // checkbox
+                    children: [
+                        {
+                            name: String,
+                            price: String,
+                        }
+                    ],
+
+                },
+                {
+                    header: String,
+                    children_type: "checkbox", // checkbox
+                    children: [],
+
+                }
+            ],
+
             rating: Number,
             is_veg: String, // yes , no
             details: String
         }
-    ],
+    ]
+
+
 }
